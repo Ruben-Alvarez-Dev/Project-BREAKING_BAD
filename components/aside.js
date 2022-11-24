3f1d9e107db1836ef89012cf9da0171d3df06b1c
imac <imac@iPro-de-imac.home>
2022-11-24 19:25:11 +0100

    feat: gallery & aside components ready

--- components/aside.js
+++ components/aside.js
@@ -2,8 +2,10 @@ import "../styles/aside.css";
 export function setupAside(element) {
     
     element.innerHTML = `
-    <div class="aside">ASIDE
-        <div class="asideTitle"></div>
+    <div class="aside">
+        <div class="asideTitle">Aside Title</div>
+        <div class="asideTool">Aside Tool 1</div>
+        <div class="asideTool">Aside Tool 2</div>
     </div>
     `
   
--- components/main.js
+++ components/main.js
@@ -2,12 +2,14 @@ import "../styles/main.css";
 export function setupMain(element) {
     
   element.innerHTML = `
-  <div class="main">MAIN
-    <div="hero">
-      
-        <span class="heroTitle">Titulo</span>
-        <span class="heroContent">Contenido</span>
-
+  <div class="main">
+    <div id="hero" class="hero">
+        <div class="heroTitle">Hero Title</div>
+        <div class="heroContent">Hero Content</div>
+    </div>
+    <div id="gallery" class="gallery">
+        <div class="galleryTitle">Gallery Title</div>
+        <div class="galleryContent">Gallery Content</div>
     </div>
   </div>
   `
